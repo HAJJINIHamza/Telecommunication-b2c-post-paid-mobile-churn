26395ef1f4342ce55aab57c2d1bbd0375e5472cc
hamza_hajjini <hamza_hajjini@MacBook-Pro-de-admin.local>
2024-10-02 09:34:28 +0100

    Added: function visualize_missing_values_per_column

--- src/eda/utils.py
+++ src/eda/utils.py
@@ -1,3 +1,22 @@
 import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
+def visualize_missing_values_per_column(df: DataFrame, figsize=(15,4)):
+    """
+    Visualize the number of missing values per columns.
+    Takes as input a DataFrame or a sample of DataFrame
+    """
+    missing_values_per_column = pd.DataFrame (
+                                            { "column": df.isna().sum().index, 
+                                               "nbr_null_values": df.isna().sum().to_list() }
+                                               )
+    
+    df_sample = missing_values_per_column
+    plt.figure(figsize=figsize)
+    fig = sns.barplot(x= df_sample.column, y=df_sample.nbr_null_values)
+    fig.set_title("nbr of missing values per column")
+    plt.xticks(rotation= 315, ha="left")
+    plt.show()
+        
