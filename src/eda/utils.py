1ae5059aba96415f2bbfcaa81d9e34e5ac8443ce
hamza_hajjini <hamza.hajjini@inwi.ma>
2024-10-07 16:29:18 +0100

    Structured column families function into a class

--- src/eda/utils.py
+++ src/eda/utils.py
@@ -26,44 +26,50 @@ def vis_perc_missing_values_per_column(df: DataFrame, figsize=(15,4)):
         raise CustomException(e, sys)
         
 
-
-        
-def get_df_columns_startswith_prefrix(df, prefix: str):
-    """
-    Retruns df columns that starts with a prefix
-    Meant for feature famillies like usagespecification_localdataservice
-    """
-    return [col for col in df.columns if col.startswith(prefix)]
-
-def get_df_columns_ends_with_sufix(df, sufix: str):
-    """
-    Retruns df columns that ends with a sufix 
-    Meant for features that occured in a certain month like : "nb_3m"
-    """
-    return [col for col in df.columns if col.endswith(sufix)]
-
-def get_list_of_df_columns_prefix(df):
-    """
-    Retruns a list of df columns prefixes to choose from 
-    in order to build feature famillies
-    """
-    try:
-        sufix = "nb_1m"
-        list_of_cols = get_df_columns_ends_with_sufix(df, sufix)
-        return [col.replace("_nb_1m", "") for col in list_of_cols]
-    except Exception as e:
-        raise CustomException(e, sys) 
-        
-def get_columns_families(df):
-    """
-    Return dictionnarie where values are column familie name (prefix)
-    and values are df.columns that belongs to this familiy
-    """
-    #Get columns families by prefix
-    prefix_list = get_list_of_df_columns_prefix(df)
+class columnsFamilies:
     
-    #Get columns families
-    columns_families = {prefix: get_df_columns_startswith_prefrix(df, prefix) for prefix in prefix_list}
-    return columns_families
+    def __init__(self, df):
+        self.df = df
+
+    def get_df_columns_startswith_prefrix(self, prefix: str):
+        """
+        Retruns df columns that starts with a prefix
+        Meant for feature famillies like usagespecification_localdataservice
+        """
+        return [col for col in self.df.columns if col.startswith(prefix)]
+
+    def get_df_columns_ends_with_sufix(self, sufix: str):
+        """
+        Retruns df columns that ends with a sufix 
+        Meant for features that occured in a certain month like : "nb_3m"
+        """
+        return [col for col in self.df.columns if col.endswith(sufix)]
+
+    def get_list_of_df_columns_prefix(self):
+        """
+        Retruns a list of df columns prefixes to choose from 
+        in order to build feature famillies
+        """
+        try:
+            sufix = "nb_1m"
+            list_of_cols = self.get_df_columns_ends_with_sufix(sufix)
+            return [col.replace("_nb_1m", "") for col in list_of_cols]
+        except Exception as e:
+            raise CustomException(e, sys) 
+
+    def get_columns_families(self):
+        """
+        Return dictionnarie where values are column familie name (prefix)
+        and values are df.columns that belongs to this familiy
+        """
+        try:
+            #Get columns families by prefix
+            prefix_list = self.get_list_of_df_columns_prefix()
+            #Get columns families
+            columns_families = {prefix: self.get_df_columns_startswith_prefrix(prefix) for prefix in prefix_list}
+            return columns_families
+        
+        except Exception as e:
+            raise CustomException(e, sys)
 
         
